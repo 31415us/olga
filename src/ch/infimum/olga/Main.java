e6c13192ad03a2c2dcf71d550e9e95d9e3e7a3fc
31415us <pius.vondaeniken@epfl.ch>
2014-03-11 00:02:51 +0100

    added some more evaluators

--- src/ch/infimum/olga/Main.java
+++ src/ch/infimum/olga/Main.java
@@ -7,7 +7,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        ImageGenerator generator = new ImageGenerator(5,"test.png", new AvgDistanceEvaluator());
+        ImageGenerator generator = new ImageGenerator(6,"test.png", new AvgWarmthDiffEvaluator());
 
         generator.generate();
     }
@@ -45,6 +45,96 @@ class AvgDistanceEvaluator implements Evaluator
         }
 
         int size = nColors.size();
+        if(size == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return acc / size;
+        }
+    }
+}
+
+class MinBrightnessDifferenceEvaluator implements Evaluator
+{
+    public int evaluate(int color, List<Integer> nColors)
+    {
+        int min = Integer.MAX_VALUE;
+
+        for(int n : nColors)
+        {
+            int diff = NBitColors.brightness(color) - NBitColors.brightness(n);
+            diff = Math.abs(diff);
+
+            if(diff < min)
+            {
+                min = diff;
+            }
+        }
+
+        return min;
+    }
+}
+
+class AvgBrightnessDifferenceEvaluator implements Evaluator
+{
+    public int evaluate(int color, List<Integer> nColors)
+    {
+        int acc = 0;
+
+        for(int n : nColors)
+        {
+            acc += Math.abs(NBitColors.brightness(color) - NBitColors.brightness(n));
+        }
+
+        int size = nColors.size();
+
+        if(size == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return acc / size;
+        }
+    }
+}
+
+class MinWarmthDiffEvaluator implements Evaluator
+{
+    public int evaluate(int color, List<Integer> nColors)
+    {
+        int min = Integer.MAX_VALUE;
+
+        for(int n : nColors)
+        {
+            int diff = NBitColors.warmth(color) - NBitColors.warmth(n);
+            diff = Math.abs(diff);
+
+            if(diff < min)
+            {
+                min = diff;
+            }
+        }
+
+        return min;
+    }
+}
+
+class AvgWarmthDiffEvaluator implements Evaluator
+{
+    public int evaluate(int color, List<Integer> nColors)
+    {
+        int acc = 0;
+
+        for(int n : nColors)
+        {
+            acc += Math.abs(NBitColors.warmth(color) - NBitColors.warmth(n));
+        }
+
+        int size = nColors.size();
+
         if(size == 0)
         {
             return 0;
