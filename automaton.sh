f916ebde35a55863cc4c47ab088fe2df1a684609
31415us <pius.vondaeniken@epfl.ch>
2014-05-02 12:50:06 +0200

    those files were moved into subdirectories

--- automaton.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for name in "Portrait of Dr. Gachet" "Three Studies of Lucian Freud" "Bal du moulin de la Galette" "Garçon à la pipe" "The Scream" "Flag" "Nude, Green Leaves and Bust"
-do
-	ant -Dname "${name}"
-	echo "Done for ${name}\n"
-done
-echo "Done !\n"
-return 0
--- src/ch/infimum/olga/Evaluator.java
+++
@@ -1,9 +0,0 @@
-
-package ch.infimum.olga;
-
-import java.util.List;
-
-public interface Evaluator
-{
-    public int evaluate(int color, List<Integer> neighbouringColors);
-}
